3dd8fd40e9fdd8edf5906717c22918fcaa4495a2
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-10-05 08:45:00 -0600

    done ica7

--- ICA7_NicW.sql
+++ ICA7_NicW.sql
@@ -89,13 +89,82 @@ order by CategoryID, ProductName
 go
 
 --q7
-
+declare @PriceMax as int = 15
+select
+	CompanyName as 'Company Name',
+	Country
+from Customers
+where CustomerID in (
+	select CustomerID
+	from Orders
+	where OrderID in (
+		select OrderID
+		from [Order Details]
+		where UnitPrice * Quantity < @PriceMax
+	)
+)
+order by Country
 go
 
 --q8
-
+declare @PriceMax as int = 15
+select
+	CompanyName as 'Company Name',
+	Country
+from Customers as outShell
+where exists (
+	select CustomerID
+	from Orders as midShell
+	where outShell.CustomerID = midShell.CustomerID and 
+	exists (
+		select OrderID
+		from [Order Details] as botShell
+		where midShell.OrderID = botShell.OrderID and
+			UnitPrice * Quantity < @PriceMax
+	)
+)
+order by Country
 go
 
 --q9
+declare @date as int = 7
+select
+	ProductName
+from Products
+where ProductID in (
+	select ProductID
+	from [Order Details]
+	where OrderID in (
+		select OrderID
+		from Orders
+		where ShippedDate > (RequiredDate + @date) and 
+			  CustomerID in (
+				select CustomerID
+				from Customers
+				where Country in ('UK', 'USA')
+			  )
+	)
+)
+order by ProductName
+go
 
+--q10
+select
+	OrderID,
+	ShipCity
+from Orders as outTable
+where OrderID in(
+	select OrderID
+	from [Order Details]
+	where ProductID in (
+		select ProductID
+		from Products
+		where SupplierID in (
+			select SupplierID
+			from Suppliers as inTable
+			where outTable.ShipCity = inTable.City
+		)
+	)
+)
+order by ShipCity
 go
