2c9fb5ca7a4b2c86ec04674570a610d18d3b9bd9
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-02 09:27:03 -0600

    Made ica 13
    Created a demo and the skeleton for the ica 13

---
+++ ICA13_NicW.sql
@@ -0,0 +1,121 @@
+--ica 13
+--Nic Wasylyshyn
+
+--q1
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica13_01'
+)
+	drop procedure ica13_01
+go
+
+create procedure ica13_01
+as
+	
+go
+
+exec ica13_01
+go
+
+--q2
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica13_02'
+)
+	drop procedure ica13_02
+go
+
+create procedure ica13_02
+as
+	
+go
+
+exec ica13_02
+go
+
+--q3
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica13_03'
+)
+	drop procedure ica13_03
+go
+
+create procedure ica13_03
+as
+	
+go
+
+exec ica13_03
+go
+
+--q4
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica13_04'
+)
+	drop procedure ica13_04
+go
+
+create procedure ica13_04
+as
+	
+go
+
+exec ica13_04
+go
+
+--q5
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica13_05'
+)
+	drop procedure ica13_05
+go
+
+create procedure ica13_05
+as
+	
+go
+
+exec ica13_05
+go
+
+--q6
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica13_06'
+)
+	drop procedure ica13_06
+go
+
+create procedure ica13_06
+as
+	
+go
+
+exec ica13_06
+go
+
+--q7
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica13_07'
+)
+	drop procedure ica13_07
+go
+
+create procedure ica13_07
+as
+	
+go
+
+exec ica13_07
+go
