1b244ee42a2dd36b1d20f038e0bf077fa185d5ed
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-07 12:41:29 -0700

    Done q2

--- ICA14_NicW.sql
+++ ICA14_NicW.sql
@@ -11,12 +11,58 @@ if exists(
 go
 
 create procedure ica14_01
+@Type as nvarchar(20),
+@Name as nvarchar(20) output,
+@Quantity as int output
 as
-	
+	select top(1)
+		@Name = p.ProductName,
+		@Quantity = od.Quantity
+	from NorthwindTraders.dbo.Products as p
+		left outer join NorthwindTraders.dbo.[Order Details] as od
+		on p.ProductID = od.ProductID
+		left outer join NorthwindTraders.dbo.Categories as c
+		on p.CategoryID = c.CategoryID
+	where c.CategoryName like @Type
+	order by Quantity desc
 go
 
-exec ica14_01
+--Exec 1
+declare @ProdType as nvarchar(20) = 'Beverages',
+		@ProdName as nvarchar(20),
+		@TopQuantity as int
+
+exec ica14_01 @ProdType, @ProdName output, @TopQuantity output
+select	@ProdType as 'Category',
+		@ProdName as 'ProductName', 
+	    @TopQuantity as 'Highest Qty'
+
+--Exec 2
+set @ProdType = 'Confections'
+
+exec ica14_01 @Type = @ProdType, @Name = @ProdName output, @Quantity = @TopQuantity output
+select	@ProdType as 'Category',
+		@ProdName as 'ProductName', 
+	    @TopQuantity as 'Highest Qty'
 go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 --q2
 if exists(
@@ -28,12 +74,56 @@ if exists(
 go
 
 create procedure ica14_02
+@Year as int,
+@Name as nvarchar(64) output,
+@AvgFreight as money output
 as
+	select top(1)
+		@Name = e.LastName + ', ' + e.FirstName,
+		@AvgFreight = avg(o.Freight)
+	from NorthwindTraders.dbo.Employees as e
+		inner join NorthwindTraders.dbo.Orders as o
+		on e.EmployeeID = o.EmployeeID
+	where datepart(year, o.OrderDate) like @Year
+	group by e.LastName, e.FirstName
+	order by avg(o.Freight) desc
 	
 go
 
-exec ica14_02
-go
+declare @myYear as int = 1996,
+		@name as nvarchar(64),
+		@freight as money
+exec ica14_02 @myYear, @name output, @freight output
+select
+	@myYear as 'Year',
+	@name as 'Name',
+	@freight as 'Biggest Avg Freight' 
+
+set @myYear = 1997
+exec ica14_02 @Year = @myYear, @Name = @name output, @AvgFreight = @freight output
+select
+	@myYear as 'Year',
+	@name as 'Name',
+	@freight as 'Biggest Avg Freight' 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 --q3
 if exists(
@@ -52,6 +142,20 @@ go
 exec ica14_03
 go
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 --q4
 if exists(
 	select *
@@ -68,6 +172,22 @@ go
 
 exec ica14_04
 go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 --q5
 if exists(
