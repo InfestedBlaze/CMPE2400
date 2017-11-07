71686c1b91391f5d2067cd79a098465182330815
Nicholas B Wasylyshyn <nwasylyshyn1@nait.ca>
2017-11-07 11:52:51 -0700

    Skeleton

--- ICA14_NicW.sql
+++ ICA14_NicW.sql
@@ -17,3 +17,71 @@ go
 
 exec ica14_01
 go
+
+--q2
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica14_02'
+)
+	drop procedure ica14_02
+go
+
+create procedure ica14_02
+as
+	
+go
+
+exec ica14_02
+go
+
+--q3
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica14_03'
+)
+	drop procedure ica14_03
+go
+
+create procedure ica14_03
+as
+	
+go
+
+exec ica14_03
+go
+
+--q4
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica14_04'
+)
+	drop procedure ica14_04
+go
+
+create procedure ica14_04
+as
+	
+go
+
+exec ica14_04
+go
+
+--q5
+if exists(
+	select *
+	from sysobjects
+	where name like 'ica14_05'
+)
+	drop procedure ica14_05
+go
+
+create procedure ica14_05
+as
+	
+go
+
+exec ica14_05
+go
