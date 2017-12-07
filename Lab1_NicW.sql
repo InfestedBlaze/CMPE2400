287d5c7909387a1358d29f36620f906a31121a4c
Nicholas Wasylyshyn <nicwasylyshyn@gmail.com>
2017-12-06 20:25:31 -0700

    Added ClassInfo

--- Lab1_NicW.sql
+++ Lab1_NicW.sql
@@ -1,14 +1,14 @@
---Lab 1 
+--Lab 1
 --Nicholas Wasylyshyn
 --Bike riders database
 
 USE [master]
 GO
 
-if exists 
+if exists
 (
-	select	* 
-	from	sysdatabases 
+	select	*
+	from	sysdatabases
 	where name='nwasylyshyn1_Riders'
 )
 		drop database nwasylyshyn1_Riders
@@ -72,8 +72,8 @@ CREATE TABLE [dbo].[Sessions](
 GO
 
 CREATE NONCLUSTERED INDEX NCI_RiderSession ON [Sessions] ([RiderID], [SessionDate])
-ALTER TABLE [Sessions] ADD 
-	CONSTRAINT FK_Sessions_BikeID FOREIGN KEY (BikeID) 
+ALTER TABLE [Sessions] ADD
+	CONSTRAINT FK_Sessions_BikeID FOREIGN KEY (BikeID)
 	REFERENCES Bikes(BikeID) ON DELETE NO ACTION
 GO
 
@@ -89,32 +89,30 @@ if exists(
 go
 create procedure PopulateBikes
 as
-	declare @looperBike as int = 0;
-	declare @looperMake as int = 0;
-	declare @looperTime as int = 0;
-	declare @maker as nvarchar(3) = 'HYS'
-	declare @timer as nvarchar(2) = 'AP'
+	declare @looperBike as int = 0
+	declare @looperMake as int = 0
+	declare @looperTime as int = 0
 
 	while @looperBike < 20
 	begin
-		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID) 
+		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID)
 		values ( RIGHT('000'+CAST(@looperBike AS VARCHAR(3)),3) + 'H-A')
-			
-		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID) 
+
+		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID)
 		values ( RIGHT('000'+CAST(@looperBike AS VARCHAR(3)),3) + 'H-P')
-			
-		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID) 
+
+		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID)
 		values ( RIGHT('000'+CAST(@looperBike AS VARCHAR(3)),3) + 'Y-A')
-			
-		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID) 
+
+		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID)
 		values ( RIGHT('000'+CAST(@looperBike AS VARCHAR(3)),3) + 'Y-P')
-		
-		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID) 
+
+		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID)
 		values ( RIGHT('000'+CAST(@looperBike AS VARCHAR(3)),3) + 'S-A')
-		
-		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID) 
+
+		insert into nwasylyshyn1_Riders.dbo.Bikes (BikeID)
 		values ( RIGHT('000'+CAST(@looperBike AS VARCHAR(3)),3) + 'S-P')
-			
+
 		set @looperBike += 1;
 	end
 go
@@ -136,29 +134,29 @@ as
 		set @ErrorMessage  = 'RemoveBike : BikeID can''t be NULL'
 		return -1
 	end
-	
-	if not exists ( 
-		select * 
-		from Bikes 
-		where BikeID = @BikeID 
+
+	if not exists (
+		select *
+		from Bikes
+		where BikeID = @BikeID
 	)
 	begin
 		set @ErrorMessage  = 'RemoveBike : ' + @BikeID + ' doesn''t exist'
 		return -1
 	end
-	
+
 	if exists (
 		select *
 		from Sessions
-		where BikeID = @BikeID 
+		where BikeID = @BikeID
 	)
 	begin
-		set @ErrorMessage  = 'RemoveBike : ‘ + @BikeID + ‘ Currently in Session' 
+		set @ErrorMessage  = 'RemoveBike : ï¿½ + @BikeID + ï¿½ Currently in Session'
 		return -1
 	end
-	
+
 	delete
-	from Bikes 
+	from Bikes
 	where BikeID = @BikeID
 	set @ErrorMessage = 'OK'
 	return 0
@@ -184,7 +182,7 @@ as
 		from Class
 		where ClassID like @ClassID
 	)
-		return -2
+		return -1
 
 	insert Riders ([Name], [ClassID])
 	values (@Name, @ClassID);
@@ -209,16 +207,16 @@ as
 		where RiderID = @RiderID
 	)
 		return -1
-	
+
 
 	declare @count as int
-	select 
+	select
 		@count = count(s.RiderID)
 	from Sessions as s
 	where s.RiderID = @RiderID
 
 	if @count > 0 and @force = 0
-		return -2
+		return -1
 
 	if @force = 1
 		delete Sessions
@@ -255,17 +253,17 @@ as
 		from Sessions
 		where BikeID = @BikeID
 	)
-		return -2
+		return -1
 
 	if @Date is null or @Date < GETDATE()
-		return -3
-	
+		return -1
+
 	if exists(
 		select BikeID
 		from Sessions
 		where BikeId = @BikeID and SessionDate = @Date --Same bike at the same time
 	)
-		return -4
+		return -1
 
 	insert Sessions ([RiderID], [BikeID], [SessionDate])
 	values (@RiderID, @BikeID, @Date)
@@ -286,22 +284,33 @@ create procedure UpdateSession
 @Date as datetime,
 @Laps as int
 as
+
+	if not exists(
+		select *
+		from Sessions
+		where
+			RiderID = @RiderID and
+			BikeID like @BikeID and
+			SessionDate = @Date
+	)
+		return -1
+
 	declare @lap as int
 
-	select 
+	select
 		@lap = Laps --Get our current number of laps
 	from Sessions
-	where 
+	where
 		BikeID like @BikeID and
 		RiderID = @RiderID and
 		SessionDate = @Date
 
 	--our current laps are less than the laps to change it to
 	--Also have a session that has laps
-	if @lap < @Laps and @lap != null 
+	if @lap < @Laps and @lap != null
 		update Sessions
 		set Laps = @lap
-		where 
+		where
 			BikeID like @BikeID and
 			RiderID = @RiderID and
 			SessionDate = @Date
@@ -336,9 +345,8 @@ as
 
 	--We have a rider in this class, don't force
 	if @rows > 0 and @force = 0
-		return -2
-	
-	if @rows > 0 and @force = 1 begin
+		return -1
+	else if @rows > 0 and @force = 1 begin
 		delete Sessions
 		where RiderID in (
 			select RiderID
@@ -373,8 +381,39 @@ if exists(
 	drop procedure ClassInfo
 go
 create procedure ClassInfo
+@ClassID as nvarchar(30),
+@RiderID as int = null
 as
-	
+	--ClassID can't be null or empty
+	if @ClassID is null or @ClassID like ''
+		return -1
+	--ClassID has to exist
+	if not exists(
+		select ClassID
+		from Class
+		where ClassID like @ClassID
+	)
+		return -1
+	--RiderID has to exist if it isn't null
+	if @RiderID is not null and not exists(
+		select RiderID
+		from Class
+		where RiderID = @RiderID
+	)
+		return -1
+
+	if @RiderID is NULL
+		select *
+		from Class as c
+			outer join Riders as r
+			on c.RiderID = r.RiderID
+		where c.ClassID like @ClassID
+	else
+		select *
+		from Class as c
+			outer join Riders as r
+			on c.RiderID = r.RiderID
+		where c.ClassID like @ClassID
 go
 
 if exists(
@@ -386,7 +425,7 @@ if exists(
 go
 create procedure ClassSummary
 as
-	
+
 go
 
 ---Error Testing-----------------------------------------
